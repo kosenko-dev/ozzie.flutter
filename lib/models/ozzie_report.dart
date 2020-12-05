7df785afbe35222d18d6405b60b1b4b31915d19d
kosenko-dev <kosenko.dev@gmail.com>
2020-12-05 18:32:52 +0300

    Russification, addding additional tab of ungroudped tests

--- lib/models/ozzie_report.dart
+++ lib/models/ozzie_report.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:meta/meta.dart';
 import 'package:ozzie/models/test_log_entry.dart';
 
@@ -6,14 +8,14 @@ import 'models.dart';
 /// Model than encapsulates different metrics for a feature report
 class OzzieReport {
   final String reportName;
-  final List<TestLogEntry> testLogEntries;
+  final LinkedHashMap<String, List<TestLogEntry>> testLogEntriesMap;
   final List<String> screenshots;
   final List<PerformanceReport> performanceReports;
   final PerformanceScore performanceScore;
 
   OzzieReport({
     @required this.reportName,
-    @required this.testLogEntries,
+    @required this.testLogEntriesMap,
     @required this.screenshots,
     @required this.performanceReports,
     @required this.performanceScore,
